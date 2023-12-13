7d902f7e62431fb868b45dbbe5f7bf7b6ce5e07f
Igor <igordsmelo@gmail.com>
2023-12-13 19:47:45 -0300

    updates in progress

--- 002_bmi_counter_life_expectancy/days_left.py
+++ 002_bmi_counter_life_expectancy/days_left.py
@@ -1,8 +1,8 @@
-# 🚨 Don't change the code below 👇
+###
 age = input("What is your current age?")
-# 🚨 Don't change the code above 👆
+###
 
-# Write your code below this line 👇
+###
 age = int(age)
 years_left = 90 - age
 months_left = years_left * 12
