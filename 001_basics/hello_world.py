12fc41818ddc1ddd6539bb66ad49c23ef61431f9
Igor <igordsmelo@gmail.com>
2023-12-16 14:36:00 -0300

    updates

---
+++ 001_basics/hello_world.py
@@ -0,0 +1,32 @@
+############################################################ 1
+
+print('Day 1 - Python Print Function')
+print('The function is declared like this:')
+print("print('what to print')")
+
+############################################################ 2
+
+print('Day 1 - String Manipulation')
+print('String Concatenation is done with the "+" sign.')
+print('e.g. print("Hello " + "world")')
+print("Hello " + "world")
+print('New lines can be created with a backslash and n.')
+
+############################################################ 3
+
+name = input("What's your name?")
+print(len(name))
+
+############################################################ 4
+
+a = input("a: ")
+b = input("b: ")
+
+####################################
+
+a, b = b, a
+
+####################################
+
+print("a: " + a)
+print("b: " + b)
